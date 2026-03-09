216954cc7ffdc7f282930ce1aec6a88e7244c4f7
NikhilNamade <nikhilnamade39@gmail.com>
2026-03-09 18:32:42 +0530

    Refactor MongoDB connection with error handling
    Updated MongoDB connection logic to include error handling.

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 const connectTomongoose = async()=>{
-    await mongoose.connect(process.env.MONGOOSE_URL)
-    console.log("Connected to DB");
+    try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
 };
 
 export default connectTomongoose;
