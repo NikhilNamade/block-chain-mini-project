93a93b9e14dd63f154a68e8335e0501361b17fa4
NikhilNamade <nikhilnamade39@gmail.com>
2026-03-09 18:49:37 +0530

    Update db.js

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectTomongoose = async()=>{
     try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGOOSE_URI);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
